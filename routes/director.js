77f93271e4b08b9a4a5f150a8a1411477b7aaf0d
abdurrahmanbas <abdurrahmanbasgoynuk1@gmail.com>
2021-03-15 17:32:03 +0300

    id ye göre yönetmen çekme endpointini yazdık

--- routes/director.js
+++ routes/director.js
@@ -1,6 +1,8 @@
 const express=require("express")
 const router =express.Router()
 
+const mongoose =require("mongoose")
+
 //Models
 const Director =require("../models/Director")
 
@@ -16,6 +18,7 @@ router.post("/",(req,res)=>{
         })
 })
 
+//tüm yönetmenler
 router.get("/",(req,res)=>{
     const promise =Director.aggregate([
         {
@@ -66,5 +69,66 @@ router.get("/",(req,res)=>{
         })
 
 })
+
+
+// yönetmen detay verileri
+router.get("/:director_id",(req,res)=>{
+    const {director_id}=req.params
+    const promise =Director.aggregate([
+        {
+          $match:{
+              "_id":mongoose.Types.ObjectId(director_id)
+          }
+        },
+        {
+            $lookup:{
+                from:"movies",
+                localField:"_id",
+                foreignField:"director_id",
+                as:"movies"
+            }
+        },
+        {
+            $unwind:{
+                path:"$movies",
+                preserveNullAndEmptyArrays:true // filmi olmayan directorlerde getirilir bu olmadan sadece filmi olanlar getirilir
+            }
+        },
+        {
+            $group:{
+                _id:{
+                    _id:"$_id",
+                    name:"$name",
+                    surname:"$surname",
+                    bio:"$bio"
+                },
+                movies:{
+                    $push:"$movies"
+                }
+            }
+        },
+        {
+            $project:{
+                _id:"$_id._id",
+                name:"$_id.name",
+                surname:"$_id.surname",
+                bio:"$_id.bio",
+                movies:"$movies"
+            }
+        }
+
+    ])
+
+    promise
+        .then(data=>{
+            res.json(data)
+        })
+        .catch(err=>{
+            res.json(err)
+        })
+
+})
+
+
 
 module.exports=router;
